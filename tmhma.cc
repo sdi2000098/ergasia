7ae0b011d92e126682b9bca3e41d7de55b7187f4
sdi2000098 <sdi2000098@di.uoa.gr>
2021-12-24 09:45:58 +0200

    kk

--- tmhma.cc
+++ tmhma.cc
@@ -9,18 +9,22 @@ Tmhma :: Tmhma(int xwros ) : Xwrhtikothta(xwros){
 
 PrwtoTmhma :: PrwtoTmhma(Tmhma * Next,int Xwros,int NSegs) : Tmhma(Xwros),EpomenoTmhma(Next){
     for ( int i = 0 ;i<Ari8mosAutokinhtwn;i++)
-        AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%NSegs,1);
+        AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%NSegs+1,0);
     EisodosTmhmatos = new PrwthEisodos(NSegs);
 };
 
 TeleutaioTmhma :: TeleutaioTmhma(Tmhma * Prev,int Xwros,int NSegs) : Tmhma(Xwros),ProhgoumenoTmhma(Prev){
     for ( int i = 0 ;i<Ari8mosAutokinhtwn;i++)
-        AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%NSegs,1);
+        AutokinhtaTouTmhmatos[i] = new Autokinhto(NSegs+1,NSegs);
     EisodosTmhmatos = new E3odos(NSegs);
 };
 
 EndiamesoTmhma :: EndiamesoTmhma(Tmhma* Prev,Tmhma* Next,int Xwros,int Thesh,int NSegs) :
     Tmhma(Xwros),EpomenoTmhma(Next),ProhgoumenoTmhma(Prev),TheshTmhmtatos(Thesh){
     for ( int i = 0 ;i<Ari8mosAutokinhtwn;i++)
-        AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%NSegs,1);
+        AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%(NSegs-TheshTmhmtatos)+TheshTmhmtatos+1,TheshTmhmtatos);
+}
+
+Tmhma :: ~Tmhma(){
+    
 }
