8dedee4469cd5a22fb71308989dea37bcf85f26c
sdi2000098 <79643724+sdi2000098@users.noreply.github.com>
2021-12-25 01:46:18 +0200

    Update tmhma.h

--- tmhma.h
+++ tmhma.h
@@ -9,27 +9,31 @@ class Tmhma{
     public :
         Tmhma(int);
         ~Tmhma();
-        virtual void Enter();
-        virtual void Exit();
-        int GetXwrhtikothta() const;
-        virtual void Pass();
-        int GetNoOfVehicles() const;
+        virtual void Enter(){};
+        virtual void Exit(){};
+        int GetXwrhtikothta() const{return Xwrhtikothta;};
+        virtual void Pass(){};
+        int GetNoOfVehicles() const{return GetNoOfVehicles};
 };
 
 class PrwtoTmhma : public Tmhma{
     private:
         Tmhma * EpomenoTmhma;
+        bool *etoimo;
     public :
         PrwtoTmhma(Tmhma *,int Xwros,int NSegs);
         ~PrwtoTmhma();
+        void Exit();
 };
 
 class TeleutaioTmhma : public Tmhma{
     private:
         Tmhma * ProhgoumenoTmhma;
+        bool *etoimo;
     public:
         TeleutaioTmhma(Tmhma *,int Xwros,int NSegs);
         ~TeleutaioTmhma();
+        void Exit();
 };
 
 class EndiamesoTmhma : public Tmhma{
@@ -37,7 +41,9 @@ class EndiamesoTmhma : public Tmhma{
         Tmhma * EpomenoTmhma;
         Tmhma * ProhgoumenoTmhma;
         int TheshTmhmtatos;
+        bool *etoimo;
     public :
         EndiamesoTmhma(Tmhma * Prohgoumeno, Tmhma * Epomeno,int Xwros,int Thesh,int NSegs );
         ~EndiamesoTmhma();
+        void Exit();
 }; 
