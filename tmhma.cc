361fce0c83d691007ed38bfb1a5fb9348c09e300
sdi2000098 <79643724+sdi2000098@users.noreply.github.com>
2021-12-25 01:45:36 +0200

    Update tmhma.cc

--- tmhma.cc
+++ tmhma.cc
@@ -25,6 +25,40 @@ EndiamesoTmhma :: EndiamesoTmhma(Tmhma* Prev,Tmhma* Next,int Xwros,int Thesh,int
         AutokinhtaTouTmhmatos[i] = new Autokinhto(rand()%(NSegs-TheshTmhmtatos)+TheshTmhmtatos+1,TheshTmhmtatos);
 }
 
-Tmhma :: ~Tmhma(){
-    
+Tmhma :: ~Tmhma(){}
+
+PrwtoTmhma :: ~PrwtoTmhma(){
+    delete EisodosTmhmatos;
+    for (int i = 0;i<Ari8mosAutokinhtwn;i++)
+        delete AutokinhtaTouTmhmatos[i];
+    delete []AutokinhtaTouTmhmatos;
 }
+
+TeleutaioTmhma :: ~TeleutaioTmhma(){
+    delete EisodosTmhmatos;
+    for (int i = 0;i<Ari8mosAutokinhtwn;i++)
+        delete AutokinhtaTouTmhmatos[i];
+    delete []AutokinhtaTouTmhmatos;
+}
+
+EndiamesoTmhma :: ~EndiamesoTmhma(){
+    delete EisodosTmhmatos;
+    for (int i = 0;i<Ari8mosAutokinhtwn;i++)
+        delete AutokinhtaTouTmhmatos[i];
+    delete []AutokinhtaTouTmhmatos;
+}
+
+void PrwtoTmhma :: Exit(){
+    for (int i = 0;i<Ari8mosAutokinhtwn;i++){
+        if (AutokinhtaTouTmhmatos[i]->E3odos() == 1){
+            Autokinhto * ToDelete = AutokinhtaTouTmhmatos[i];
+            for (int j = i;j<Ari8mosAutokinhtwn-1;j++)
+                AutokinhtaTouTmhmatos[j]=AutokinhtaTouTmhmatos[j+1];
+            delete ToDelete;
+            Ari8mosAutokinhtwn--;
+        }
+    }
+}
+
+
+
