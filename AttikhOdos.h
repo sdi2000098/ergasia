0a8d12b4750e6de8846228c92f45d81643be3d8b
sdi2000098 <79643724+sdi2000098@users.noreply.github.com>
2022-01-08 15:31:45 +0200

    Create AttikhOdos.h

---
+++ AttikhOdos.h
@@ -0,0 +1,11 @@
+#include <tmhma.h>
+
+class AttikhOdos{
+    private: 
+        int NSegs,Plh8osOxhmatwn;
+        Tmhma ** TmhmataAttikhs;
+    public :
+        AttikhOdos(int NSegs);
+        ~AttikhOdos();
+        Operate();
+};
