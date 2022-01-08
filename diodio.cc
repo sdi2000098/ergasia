8b00b7eff490160965b4a9da3f261a05522cb283
sdi2000098 <79643724+sdi2000098@users.noreply.github.com>
2022-01-08 15:32:17 +0200

    Update diodio.cc

--- diodio.cc
+++ diodio.cc
@@ -1,14 +1,14 @@
 #include "diodio.h"
 
-Diodio::Diodio(int NSegs){
+Diodio::Diodio(int NSegs,int TheshDiodiou){
     Plh8osAutokinhtwn= rand()%10+1;
-    AutokinhtaDiodiou = new Autokinhto *[Plh8osAutokinhtwn];
+    AutokinhtaDiodiou = new Autokinhto*[Plh8osAutokinhtwn];
     for ( int i = 0 ;i<Plh8osAutokinhtwn;i++)
-        AutokinhtaDiodiou[i] = new Autokinhto(rand()%NSegs +1,-1);
+        AutokinhtaDiodiou[i] = new Autokinhto(rand()%(NSegs+1-TheshDiodiou) +TheshDiodiou+1,-1);
 }
 
 void Diodio::EisodosAutokinhtou(const Autokinhto & Neo){
-    if ( (AutokinhtaDiodiou = (Autokinhto ** )realloc(AutokinhtaDiodiou,Plh8osAutokinhtwn+1) )== NULL){
+    if ( (AutokinhtaDiodiou = (Autokinhto ** )realloc(AutokinhtaDiodiou,(Plh8osAutokinhtwn+1)*sizeof(Autokinhto*) ))== NULL){
         cout << "Provlhma sthn desmeush mnhmhs\n";
         exit(-1);
     }
@@ -16,18 +16,23 @@ void Diodio::EisodosAutokinhtou(const Autokinhto & Neo){
 }
 
 Autokinhto & Diodio::E3odosAutokinhtou() {
-    Autokinhto  PouVgainei = *AutokinhtaDiodiou[0];
+    if (Plh8osAutokinhtwn == 0){
+        Autokinhto PouVgainei(-1,-1);
+        return PouVgainei;
+    }
+    Autokinhto  PouVgainei(*AutokinhtaDiodiou[0]);
     delete AutokinhtaDiodiou[0];
     for ( int i = 1 ; i < Plh8osAutokinhtwn;i++)
         AutokinhtaDiodiou[i-1] = AutokinhtaDiodiou[i];
-    if ( (AutokinhtaDiodiou = (Autokinhto ** )realloc(AutokinhtaDiodiou,Plh8osAutokinhtwn-1) )== NULL){
+    if ( (AutokinhtaDiodiou = (Autokinhto ** )realloc(AutokinhtaDiodiou,(Plh8osAutokinhtwn-1)*sizeof(Autokinhto*) ) )== NULL){
         cout << "Provlhma sthn desmeush mnhmhs\n";
         exit(-1);
     }
+    Plh8osAutokinhtwn--;
     return PouVgainei;
-} 
-
-DiodioMeYpallhlo::DiodioMeYpallhlo(int NSegs) : Diodio(NSegs){
+}  
+ 
+DiodioMeYpallhlo::DiodioMeYpallhlo(int NSegs,int TheshDiodiou) : Diodio(NSegs,TheshDiodiou){
     cout << "Kataskeuasthke ena diodio me ypallhlo\n";
 }
 
@@ -35,11 +40,11 @@ DiodioMeYpallhlo::~DiodioMeYpallhlo(){
     cout << "KAtastrafhke ena diodio me ypallhlo\n";
 }
 
-HelktronikoDiodio::HelktronikoDiodio(int NSegs) : Diodio(NSegs){
+HelktronikoDiodio::HelktronikoDiodio(int NSegs,int TheshDiodiou) : Diodio(NSegs,TheshDiodiou){
     cout << "Kataskeuasthke ena hlektroniko diodio \n";
 }
 
 HelktronikoDiodio::~HelktronikoDiodio(){
-    cout << "KAtastrafhke ena hlektroniko diodio\n";
+    cout << "Katastrafhke ena hlektroniko diodio\n";
 }
 
